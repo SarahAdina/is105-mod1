1accae6bd6146ed9414dbe58bb30f15de2e4857e
SarahAdina <sakristian@uia.no>
2022-01-26 10:37:46 +0100

    second commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main 
+
+import "rsc.io/quote"
+import "fmt"
+
+
+func main () {
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+}
+
